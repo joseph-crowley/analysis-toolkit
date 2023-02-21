b16c9c5fbed7c4813be2b57a1ea9bd9c28e347cc
Joe Crowley <crowley@ucsb.edu>
2023-02-20 20:27:57 -0800

    fix file input and call to c++

--- python/analysis.py
+++ python/analysis.py
@@ -1,10 +1,11 @@
 """
 analysis.py
 
-This script provides the main functions for performing event-by-event analysis of particle physics data from CMS.
+This script provides the main functionality for performing event-by-event analysis of particle physics data from CMS.
 
 Functions:
-    - process_chain: This function takes a chain of ROOT files and processes them event-by-event.
+    - main: This function is the main entry point for the script. It loads the samples, creates the TChains, and processes the events.
+        depends on: C++ process_chain: This function takes a chain of ROOT files and processes them event-by-event.
 
 Author: Joe Crowley
 Date: 2023-02-20
@@ -16,15 +17,36 @@ import json
 
 import helpers as hf
 
-r.gSystem.Load('../cpp/loopers/analyze_bjets.so')
+def main():
+    # Load the C++ code that will be used to process the events
+    root.gSystem.Load('../cpp/loopers/analyze_bjets.so')
+    
+    # Load the samples
+    samples_json = '../config/samples_MC_Run2.json'
+    with open(samples_json,'r') as f:
+        samples = json.load(f)
+    
+    # Create a dictionary of TChains for each sample
+    map_sample_to_category = hf.map_sample_to_category
+    ch = {}
 
-samples_json = '../config/samples_MC_Run2.json'
-with open(samples_json,'r') as f:
-    samples = json.load(f)
+    # Loop over the samples and add the files to the TChains
+    for name,sample in samples:
+        for period in sample: 
+            # Create a string that will be used to identify the sample
+            sample_str = period + "_" + map_sample_to_category[name]
 
-for name,sample in samples:
-    ch = root.TChain("Events")
+            # Create a TChain for the sample if it doesn't already exist
+            if sample_str not in ch:
+                ch[sample_str] = root.TChain("Events")
 
-    for file_ in hf.get_files(sample['2017']['paths']): ch.Add(file_)
+            # Add the files to the TChain
+            for file_ in hf.get_files(sample[period]['paths']): 
+                ch[sample_str].Add(file_)
+    
+    # Process the TChains
+    for sample_str in ch:
+        root.process_chain(ch[sample_str], sample_str)
 
-    root.process_chain(ch, '2017_'+name)
+if __name__ == "__main__":
+    main()
